a5cf02f966bfd93fa175d543b53f8acc15271d3e
Olli Kortelahti <okortelahti@gmail.com>
2023-09-05 17:14:14 +0300

    Initial test

--- lib/Web/Google/Sheets.hs
+++ lib/Web/Google/Sheets.hs
@@ -4,8 +4,11 @@ module Web.Google.Sheets
 
     -- * Types
   , module Web.Google.Sheets.Types
+    -- * Re-exports
+  , MonadHttp(..), runReq, defaultHttpConfig
   )
 where
 
 import Web.Google.Sheets.Spreadsheets.Values
 import Web.Google.Sheets.Types
+import Network.HTTP.Req (MonadHttp(..), runReq, defaultHttpConfig)
--- test/Main.hs
+++ test/Main.hs
@@ -1,9 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
+import Data.ByteString (StrictByteString)
+import Data.Text (Text, pack)
+import Data.Text.Encoding (encodeUtf8)
 import System.Environment (getEnv)
+import Test.Hspec
+import Web.Google.Sheets
 
-main :: IO ()
+getToken :: IO StrictByteString
+getToken = encodeUtf8 . pack <$> getEnv "AUTH_TOKEN"
+
 main = do
-  env <- getEnv "AUTH_TOKEN"
-  print env
-  error "sorry"
+  hspec $ before getToken $ do
+    describe "appendValues" $ do
+      it "appends a value to the sheet" $ \token -> do
+        let range = RangeWithSheetName (Just (ColumnRange 1 1)) "ghc-9.2.8"
+        runReq
+          defaultHttpConfig
+          (appendValues token "1PHUwechIwIa2lyw522dUbsvdVgh-6-eJSz_jVXuAKgw" range Raw [["Moro" :: Text, "Moro"]])
+            `shouldReturn` ()
+
+-- main :: IO ()
+-- main = do
+--   env <- getEnv "AUTH_TOKEN"
+--   print env
+--   error "sorry"
