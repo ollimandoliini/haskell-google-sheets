9ef875073c1909d76f039f2d9ddb3787a9f82efe
Olli Kortelahti <okortelahti@gmail.com>
2023-09-06 08:43:33 +0300

    Fix test

--- test/Main.hs
+++ test/Main.hs
@@ -8,6 +8,7 @@ import Data.Text.Encoding (encodeUtf8)
 import System.Environment (getEnv)
 import Test.Hspec
 import Web.Google.Sheets
+import Web.Google.Sheets.Spreadsheets.Values.SheetValue (WriteSheetValue (..))
 
 getToken :: IO StrictByteString
 getToken = encodeUtf8 . pack <$> getEnv "AUTH_TOKEN"
@@ -16,11 +17,16 @@ main = do
   hspec $ before getToken $ do
     describe "appendValues" $ do
       it "appends a value to the sheet" $ \token -> do
-        let range = RangeWithSheetName (Just (ColumnRange 1 1)) "ghc-9.2.8"
-        runReq
-          defaultHttpConfig
-          (appendValues token "1PHUwechIwIa2lyw522dUbsvdVgh-6-eJSz_jVXuAKgw" range Raw [["Moro" :: Text, "Moro"]])
-          `shouldReturn` ()
+        let range = RangeWithSheetName (Just (FullRange 0 0 2 0)) "ghc-9.2.8"
+            request =
+              updateValues
+                token
+                Nothing
+                "1PHUwechIwIa2lyw522dUbsvdVgh-6-eJSz_jVXuAKgw"
+                range
+                UserEntered
+                [[SheetDouble 10, SheetBool True, SheetString "Moro"]]
+        runReq defaultHttpConfig request `shouldReturn` ()
 
 -- main :: IO ()
 -- main = do
