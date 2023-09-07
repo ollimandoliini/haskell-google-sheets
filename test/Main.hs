336be20fd55b51b26acfd640a30b05c45a3a6365
Olli Kortelahti <okortelahti@gmail.com>
2023-09-07 13:13:39 +0300

    Set env variables in Github

--- test/Main.hs
+++ test/Main.hs
@@ -5,31 +5,26 @@ module Main (main) where
 import Data.ByteString (StrictByteString)
 import Data.Text (Text, pack)
 import Data.Text.Encoding (encodeUtf8)
-import System.Environment (getEnv)
+import System.Environment
 import Test.Hspec
 import Web.Google.Sheets
 import Web.Google.Sheets.Spreadsheets.Values.SheetValue (WriteSheetValue (..))
 
-getToken :: IO StrictByteString
-getToken = encodeUtf8 . pack <$> getEnv "AUTH_TOKEN"
-
+main :: IO ()
 main = do
-  hspec $ before getToken $ do
+  token <- encodeUtf8 . pack <$> getEnv "AUTH_TOKEN"
+  spreadSheetId <- pack <$> getEnv "SPREADSHEET_ID"
+  sheetName <- pack <$> getEnv "SHEET_NAME"
+  hspec $ do
     describe "appendValues" $ do
-      it "appends a value to the sheet" $ \token -> do
-        let range = RangeWithSheetName (Just (FullRange 0 0 2 0)) "ghc-9.2.8"
+      it "appends a value to the sheet" $ do
+        let range = RangeWithSheetName (Just (FullRange 0 0 2 0)) sheetName
             request =
               updateValues
                 token
                 Nothing
-                "1PHUwechIwIa2lyw522dUbsvdVgh-6-eJSz_jVXuAKgw"
+                spreadSheetId
                 range
                 UserEntered
                 [[SheetDouble 10, SheetBool True, SheetString "Moro"]]
         runReq defaultHttpConfig request `shouldReturn` ()
-
--- main :: IO ()
--- main = do
---   env <- getEnv "AUTH_TOKEN"
---   print env
---   error "sorry"
